633f99f6b122e7191be6aa9f4c2021a93eea51e1
Elizabeth <elizabethborokinni@gmail.com>
2023-01-20 00:37:26 +0100

    created blog controller

---
+++ Controller/blogController.js
@@ -0,0 +1,15 @@
+const blogModel = require("../Model/blog");
+
+const getAllBlogs = async(req, res, next)=>{
+   try{
+    const blogs = await blogModel.find()
+
+    if(!blogs){
+        return res.status(500).send("No blogs!")
+    }
+    
+    res.status(200).send(blogs)
+   }catch(error){
+    res.status(400).send(error)
+   }
+}
