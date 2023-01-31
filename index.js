d9dfbb67f1588d418eb29c029ef574849b7596ea
Elizabeth <elizabethborokinni@gmail.com>
2023-01-31 14:02:24 +0100

    rate limiting

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const {connectToMongodb} = require("./database");
 const userRoute = require("./route/userRoute");
 const blogRoute = require("./route/blogRoute");
+const rateLimit = require("express-rate-limit");
 const helmet = require('helmet');
 
 const app = express();
@@ -10,8 +11,17 @@ const PORT = process.env.PORT;
 
 connectToMongodb()
 
+// rate limiting
+const limiter = rateLimit({
+	windowMs: 0.5 * 60 * 1000, // 15 minutes
+	max: 4, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
 app.use(express.json());
 app.use(helmet());
+app.use(limiter);
 
 app.use("/user", userRoute);
 app.use("/blog", blogRoute);
